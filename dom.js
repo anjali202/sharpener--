0b636ebc2ab74cebf5967440f61b2f41e9927887
anjali202 <goswamianjali744>
2023-02-07 13:50:04 +0530

    adding by tagname

--- dom.js
+++ dom.js
@@ -22,10 +22,32 @@
 
 
 //GETELEMENTSBYCLASSNAME  //
-var items = document.getElementsByClassName('list-group-item');
+/* var items = document.getElementsByClassName('list-group-item');
 console.log(items);
 console.log(items[2]);
 items[2].textContent = 'Hello' ;
 items[2].style.fontWeight = 'bold';
 items[2].style.backgroundColor = 'green';
+
+for (var i=0; i < items.length; i++)
+{
+    li[i].style.backgroundColor = "#f4f4f4";
+}
+
+ */
+
+//QUERY SELECTOR //
+var header = document.querySelector(' #main-header');
+header.style.borderbottom ='solid 4px #ccc';
+
+//var input = document.querySelector('input');
+//input.value = 'Hello World'
+
+//var input = document.querySelector('input[type="submit"]');
+//SubmitEvent.value ="SEND"
+
+//var item = document.querySelector('.list-group-item');
+//item.style.color = 'red';
+
+
 
