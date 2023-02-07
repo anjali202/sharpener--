892b6d85e295fdef02303d03b77900e158611bcc
anjali202 <goswamianjali744>
2023-02-07 10:17:42 +0530

    adding by classname

--- dom.js
+++ dom.js
@@ -25,6 +25,7 @@
 var items = document.getElementsByClassName('list-group-item');
 console.log(items);
 console.log(items[2]);
-items[2].textContent = 'Hello 2' ;
+items[2].textContent = 'Hello' ;
 items[2].style.fontWeight = 'bold';
-items[2].style.color= 'green';
+items[2].style.backgroundColor = 'green';
+
